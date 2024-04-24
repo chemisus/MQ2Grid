4e4cdfa064e3d7ab903b838da22a8e5859c41ccd
Terrence Howard <chemisus@gmail.com>
2024-04-24 13:27:53 -0600

    const

--- MQ2Grid.cpp
+++ MQ2Grid.cpp
@@ -21,23 +21,23 @@ static inline int InGame() {
 
 
 namespace utils {
-	int GetSkillLevel(int skill) {
+	int GetSkillLevel(const int skill) {
 		return GetPcProfile()->Skill[skill];
 	}
 
-	std::string GetArgString(char* args, int offset) {
+	std::string GetArgString(const char* args, const int offset) {
 		char arg[MAX_STRING];
 		GetArg(arg, args, offset);
 		return std::string{ arg };
 	}
 
-	float GetArgFloat(char* args, int offset) {
+	float GetArgFloat(const char* args, const int offset) {
 		char arg[MAX_STRING];
 		GetArg(arg, args, offset);
 		return (float)atof(arg);
 	}
 
-	int GetArgInt(char* args, int offset) {
+	int GetArgInt(const char* args, const int offset) {
 		char arg[MAX_STRING];
 		GetArg(arg, args, offset);
 		return atoi(arg);
@@ -47,8 +47,8 @@ namespace utils {
 		va_list vaList;
 		va_start(vaList, szFormat);
 
-		int len = _vscprintf(szFormat, vaList) + 1 + 32;
-		auto out = std::make_unique<char[]>(len);
+		const int len = _vscprintf(szFormat, vaList) + 1 + 32;
+		const auto out = std::make_unique<char[]>(len);
 		char* szOutput = out.get();
 
 		vsprintf_s(szOutput, len, szFormat, vaList);
@@ -59,8 +59,8 @@ namespace utils {
 		va_list vaList;
 		va_start(vaList, szFormat.c_str());
 
-		int len = _vscprintf(szFormat.c_str(), vaList) + 1 + 32;
-		auto out = std::make_unique<char[]>(len);
+		const int len = _vscprintf(szFormat.c_str(), vaList) + 1 + 32;
+		const auto out = std::make_unique<char[]>(len);
 		char* szOutput = out.get();
 
 		vsprintf_s(szOutput, len, szFormat.c_str(), vaList);
@@ -71,17 +71,17 @@ namespace utils {
 		va_list vaList;
 		va_start(vaList, szFormat);
 
-		int len = _vscprintf(szFormat, vaList) + 1 + 32;
-		auto out = std::make_unique<char[]>(len);
+		const int len = _vscprintf(szFormat, vaList) + 1 + 32;
+		const auto out = std::make_unique<char[]>(len);
 		char* szOutput = out.get();
 
 		vsprintf_s(szOutput, len, szFormat, vaList);
 		EzCommand(szOutput);
 	}
 
-	static EQZoneInfo* GetZoneByID(EQZoneIndex id) { return pWorldData->ZoneArray[id]; }
-	static EQZoneInfo* GetZoneByShortName(char* name) { return GetZoneByID(GetZoneID(name)); }
-	static EQZoneInfo* GetZoneByShortName(std::string name) { return GetZoneByID(GetZoneID(name.c_str())); }
+	static EQZoneInfo* GetZoneByID(const EQZoneIndex id) { return pWorldData->ZoneArray[id]; }
+	static EQZoneInfo* GetZoneByShortName(const char* name) { return GetZoneByID(GetZoneID(name)); }
+	static EQZoneInfo* GetZoneByShortName(const std::string& name) { return GetZoneByID(GetZoneID(name.c_str())); }
 	static EQZoneInfo* GetCurrentZone() { return GetZoneByID(GetCharInfo()->zoneId); }
 }
 
@@ -91,8 +91,8 @@ struct Box {
 	int width = 8;
 	int height = 8;
 
-	Box() {}
-	Box(int row, int col, int width, int height) : row(row), col(col), width(width), height(height) {}
+	Box() = default;
+	Box(const int row, const int col, const int width, const int height) : row(row), col(col), width(width), height(height) {}
 };
 
 struct Screen {
@@ -106,16 +106,16 @@ struct Grid {
 
 	Box focus;
 
-	void apply(Box& box) {
-		HWND window = GetEQWindowHandle();
+	void apply(const Box& box) const {
+		const HWND window = GetEQWindowHandle();
 		WINDOWPLACEMENT placement{};
 		GetWindowPlacement(window, &placement);
 
-		int top = box.row * height;
-		int left = box.col * width;
+		const int top = box.row * height;
+		const int left = box.col * width;
 
-		int right = left + box.width * width;
-		int bottom = top + box.height * height;
+		const int right = left + box.width * width;
+		const int bottom = top + box.height * height;
 
 		if (placement.rcNormalPosition.left == left
 			&& placement.rcNormalPosition.right == right
@@ -138,17 +138,17 @@ Grid grid;
 Screen screen;
 
 void apply() {
-	HWND window = GetEQWindowHandle();
+	const HWND window = GetEQWindowHandle();
 	WINDOWPLACEMENT placement{};
 	GetWindowPlacement(window, &placement);
 
-	Box& box = GetFocus() == window && screen.focus ? grid.focus : screen.box;
+	const Box& box = GetFocus() == window && screen.focus ? grid.focus : screen.box;
 
-	int top = box.row * grid.height;
-	int left = box.col * grid.width;
+	const int top = box.row * grid.height;
+	const int left = box.col * grid.width;
 
-	int right = left + box.width * grid.width;
-	int bottom = top + box.height * grid.height;
+	const int right = left + box.width * grid.width;
+	const int bottom = top + box.height * grid.height;
 
 	if (placement.rcNormalPosition.left == left
 		&& placement.rcNormalPosition.right == right
@@ -167,28 +167,28 @@ void apply() {
 }
 
 void saveGrid() {
-	std::string file = INIFileName;
-	std::string section = GetServerShortName();
+	const std::string file = INIFileName;
+	const std::string section = GetServerShortName();
 
-	std::string key = "GRID";
-	std::string value = utils::sprintf("GRID %i %i %i %i %i %i", grid.width, grid.height, grid.focus.width, grid.focus.height, grid.focus.row, grid.focus.col);
+	const std::string key = "GRID";
+	const std::string value = utils::sprintf("GRID %i %i %i %i %i %i", grid.width, grid.height, grid.focus.width, grid.focus.height, grid.focus.row, grid.focus.col);
 
 	WritePrivateProfileString(section, key, value, file);
 	WriteChatf("save grid %s", value.c_str());
 }
 
 void loadGrid() {
-	std::string file = INIFileName;
-	std::string section = GetServerShortName();
+	const std::string file = INIFileName;
+	const std::string section = GetServerShortName();
 
-	std::string key = "GRID";
-	std::string value = GetPrivateProfileString(section, key, "", file);
+	const std::string key = "GRID";
+	const std::string value = GetPrivateProfileString(section, key, "", file);
 
 	if (value == "") {
 		return;
 	}
 
-	char* args = (char*)value.c_str();
+	const char* args = value.c_str();
 
 	grid.width = utils::GetArgInt(args, 2);
 	grid.height = utils::GetArgInt(args, 3);
@@ -199,28 +199,28 @@ void loadGrid() {
 }
 
 void saveScreen() {
-	std::string file = INIFileName;
-	std::string section = GetServerShortName();
+	const std::string file = INIFileName;
+	const std::string section = GetServerShortName();
 
-	std::string key = GetCharInfo()->Name;
-	std::string value = utils::sprintf("%s %i %i %i %i %i", GetCharInfo()->Name, screen.box.width, screen.box.height, screen.box.row, screen.box.col, screen.focus);
+	const std::string key = GetCharInfo()->Name;
+	const std::string value = utils::sprintf("%s %i %i %i %i %i", GetCharInfo()->Name, screen.box.width, screen.box.height, screen.box.row, screen.box.col, screen.focus);
 
 	WritePrivateProfileString(section, key, value, file);
 	WriteChatf("save box %s", value.c_str());
 }
 
 void loadScreen() {
-	std::string file = INIFileName;
-	std::string section = GetServerShortName();
+	const std::string file = INIFileName;
+	const std::string section = GetServerShortName();
 
-	std::string key = GetCharInfo()->Name;
-	std::string value = GetPrivateProfileString(section, key, "", file);
+	const std::string key = GetCharInfo()->Name;
+	const std::string value = GetPrivateProfileString(section, key, "", file);
 
 	if (value == "") {
 		return;
 	}
 
-	char* args = (char*)value.c_str();
+	const char* args = value.c_str();
 
 	screen.box.width = utils::GetArgInt(args, 2);
 	screen.box.height = utils::GetArgInt(args, 3);
@@ -264,13 +264,13 @@ std::map<std::string, void(*)(PSPAWNINFO pChar, PCHAR args)> commands{
 };
 
 void addCommands() {
-	for (auto it : commands) {
+	for (const auto& it : commands) {
 		AddCommand(it.first.c_str(), it.second);
 	}
 }
 
 void removeCommands() {
-	for (auto it : commands) {
+	for (const auto& it : commands) {
 		RemoveCommand(it.first.c_str());
 	}
 }
@@ -280,10 +280,10 @@ class Timer {
 	std::chrono::milliseconds duration;
 
 public:
-	Timer(std::chrono::milliseconds duration) : duration(duration) {}
+	Timer(const std::chrono::milliseconds duration) : duration(duration) {}
 
 	bool ready() {
-		auto now = std::chrono::steady_clock::now();
+		const auto now = std::chrono::steady_clock::now();
 		if (now < point) {
 			return false;
 		}
